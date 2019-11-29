a43a80f16db48ca1a9a28860836002f6ca4613f8
AlbertEV <39110818+AlbertEV@users.noreply.github.com>
2019-11-29 16:23:01 -0400

    Se cambio el nombre de algunas variables
    Para mejorar la lectura del codigo

--- main.cpp
+++ main.cpp
@@ -4,44 +4,42 @@
 #include <stdio.h>
 #include <string.h>
 
-
 using namespace std;
 
-const string VuelosPlantilla = "ID";
 typedef list<string> TipoLista;
-
 
 class ManejarArchivo
 {
-private:
     char const *FicheroNombre;
     fstream Fichero;
+private:
+    bool ExisteFichero(string Nombre);
 
 public:
     bool EstablecerNombreFichero(string Nombre);
-    void AgregarUsuario(string User);
-    void CrearArchivo(string datos);
-    TipoLista Leer();
+    void CrearFichero(string datos);
+    void GuardarFichero(string User);
+    TipoLista CargarFichero();
 
 };
 
 bool ManejarArchivo::EstablecerNombreFichero(string Nombre){
-       Fichero.open(Nombre.c_str(), ios::in);
-       if(Fichero.is_open()){
-           //cout<<"Fichero, "<<Nombre<< "Abierto con Exito\n";
-           FicheroNombre=Nombre.c_str();
-           Fichero.close();
-           return true;
-       }else{
-           //cout<<"Fichero, "<<Nombre<< "No se Pudo Abrir\n";
-           FicheroNombre=Nombre.c_str();
-           Fichero.close();
-           return false;
-    }
-
+	Fichero.open(Nombre.c_str(), ios::in);
+	FicheroNombre=Nombre.c_str();
+	return ExisteFichero(Nombre);
 }
 
-void ManejarArchivo::CrearArchivo(string DatosCrearFichero){
+bool ManejarArchivo::ExisteFichero(string Nombre){
+	if(Fichero.is_open()){
+       Fichero.close();
+	   return true;
+	}else{
+	   Fichero.close();
+	   return false;
+	}
+}
+
+void ManejarArchivo::CrearFichero(string DatosCrearFichero){
     Fichero.open(FicheroNombre, ios::in);
     if(Fichero.is_open()){
        cout<<"Fichero, "<<FicheroNombre<< " Ya Existe\n";
@@ -52,33 +50,30 @@ void ManejarArchivo::CrearArchivo(string DatosCrearFichero){
     Fichero.close();
 }
 
-void ManejarArchivo::AgregarUsuario(string User){
-    TipoLista Lista = Leer();
+void ManejarArchivo::GuardarFichero(string User){
+    TipoLista Lista = CargarFichero();
     if(Lista.empty()){
+            cout<<"Error!, La lista esta vacia\n";
         return;
     }
     Fichero.open(FicheroNombre,ios::out);
     Lista.push_back(User);
     list<string>::iterator iterador=Lista.begin();
     for(; iterador!=Lista.end(); iterador++){
-        Fichero<<*iterador<<"\n"; // agregamos una linea de la lista al archivo
+        if(*iterador != ""){
+            Fichero<<*iterador<<"\n"; // Se agrega una linea al fichero
+        }
     }
     Fichero.close();
 }
 
-TipoLista ManejarArchivo::Leer(){
+TipoLista ManejarArchivo::CargarFichero(){
     TipoLista Lista;
     string buffer;
     Fichero.open(FicheroNombre, ios::in);
-    if(Fichero.fail()){
-       cout<<"Error!, "<<FicheroNombre<<" No existe o esta vacio.\n";
-       return Lista;
-    }
-    while(!Fichero.eof()){
+    while(!Fichero.eof() && Fichero.is_open()){
         getline(Fichero, buffer);
-        if(buffer!=""){
-            Lista.push_back(buffer); // agregamos la linea a la Lista
-        }
+        Lista.push_back(buffer); // Se lee una linea del fichero
     }
     Fichero.close();
     return Lista;
@@ -89,65 +84,61 @@ TipoLista ManejarArchivo::Leer(){
 int main()
 {
     ManejarArchivo Reservar;
-    TipoLista n;
-    TipoLista z;
-    string Plantilla_Vuelos[5] = {"ID: ", "Origen: ", "Destino: ", "Distancia: ","####################### PASAJEROS ########################  \n"};
+    TipoLista Lista;
+    string Plantilla_Vuelos[5] = {"ID: ", "Origen: ", "Destino: ", "Distancia: ","####################### PASAJEROS ########################"};
     string Plantilla_Usuario[4] = {"ID_Pasaporte: ", "Nombres: ", "Apellidos: ", "Año Nacimiento: "};
-    string in;
-    string send="";
-    int opt=0;
+    string LeidoDesdeConsola;
+    string DatosParaGuardar="";
+    int ElejirOpcion = 0;
 
     cout<<"1. Crear Registro de vuelo.\n";
     cout<<"2. Agregar Pasajeros.\n";
     cout<<"3. Ver Registro de Vuelo.\n";
-    cin>>opt;
+    cin>> ElejirOpcion;
 
-    if(opt == 1){
-    cout<<"Ingrese el ID del vuelo: ";
-    cin>>in;
-        if(Reservar.EstablecerNombreFichero(in + ".txt")){
-            cout<<"ID de Vuelo, "<<in<< " Ya Existe\n";
-        }
-        else{
-            send=(Plantilla_Vuelos[0] + in)+"\n";
-            for(int i=1; i<4; i++){
-                cout<<Plantilla_Vuelos[i];
-                cin>>in;
-                send=send + (Plantilla_Vuelos[i] + in)+"\n";
-            }
-            send=send+Plantilla_Vuelos[4];
-            Reservar.CrearArchivo(send);
-        }
+    if(ElejirOpcion == 1){
+    cout<< "Ingrese el ID del vuelo: ";
+    cin>> LeidoDesdeConsola;
+    if(Reservar.EstablecerNombreFichero(LeidoDesdeConsola + ".txt")){
+        cout<< "ID de Vuelo, "<< LeidoDesdeConsola << ".txt Ya Existe\n";
+    }
+    else{
+	    DatosParaGuardar = (Plantilla_Vuelos[0] + LeidoDesdeConsola)+"\n";
+	    for(int i=1; i<4; i++){
+	        cout<< Plantilla_Vuelos[i];
+	        cin>> LeidoDesdeConsola;
+	        DatosParaGuardar = DatosParaGuardar + (Plantilla_Vuelos[i] + LeidoDesdeConsola) + "\n";
+    	}
+    DatosParaGuardar = DatosParaGuardar + Plantilla_Vuelos[4];
+    cout<< DatosParaGuardar;
+    Reservar.CrearFichero(DatosParaGuardar);
+    }
     }
 
-
-    if(opt == 2){
-    cout<<"Ingrese el ID del Vuelo: ";
-    cin>>in;
-    if(Reservar.EstablecerNombreFichero(in + ".txt")){
-           cout<<"Ingrese el Numro de Pasaporte: ";
-           cin>>in;
-           Reservar.AgregarUsuario(in);
-        }
-        else{
-            cout<<"ID de Vuelo, "<<in<< " Ya Existe\n";
-        }
-
+    if(ElejirOpcion == 2){
+	cout<< "Ingrese el ID del Vuelo: ";
+	cin>> LeidoDesdeConsola;
+	if(Reservar.EstablecerNombreFichero(LeidoDesdeConsola + ".txt")){
+	       cout<< "Ingrese el Numero de Pasaporte: ";
+	       cin>> LeidoDesdeConsola;
+	       Reservar.GuardarFichero(LeidoDesdeConsola);
+	    }
+	    else{
+	        cout<< "Error!, "<< LeidoDesdeConsola << ".txt No Exsite\n";
+	    }
     }
 
-
-    if(opt == 3){
+    if(ElejirOpcion == 3){
     cout<<"Ingrese el ID del Vuelo: ";
-    cin>>in;
-    Reservar.EstablecerNombreFichero(in+".txt");
-    n = Reservar.Leer();
+    cin>>LeidoDesdeConsola;
+    Reservar.EstablecerNombreFichero(LeidoDesdeConsola+".txt");
+    Lista = Reservar.CargarFichero();
     list<string>::iterator iterador;
-
-    iterador=n.begin();
-    for(; iterador!=n.end(); iterador++){
+    iterador=Lista.begin();
+    for(; iterador!=Lista.end(); iterador++){
         cout<<*iterador<<endl;
     }
     }
 
-
     return 0;
+}
